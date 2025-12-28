302170ad8818dabf7f6b45185807bdc9f7b05138
Diego <diegolozanodev@gmail.com>
2025-12-27 23:45:45 -0500

    Add new dashboard, patients, and profiles apps; remove old dashboard files

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.dashboard',
+    'apps.patients',
+    'apps.profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
