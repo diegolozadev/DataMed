b0573c3c1c0d4d59122ebea6428fc986ce865d8b
Diego Lozano <diegolozadev@gmail.com>
2026-01-14 17:52:40 -0500

    Add new template 'patient_detail'

--- apps/patients/urls.py
+++ apps/patients/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.patients_list, name='patients_list'),
     path('create/', views.create_patient, name='create_patient'),
+    path('<int:patient_id>/', views.patient_detail, name='patient_detail'),
 ]
--- apps/patients/views.py
+++ apps/patients/views.py
@@ -23,3 +23,21 @@ def create_patient(request):
         form = PatientForm()
 
     return render(request, 'patients/create_patient.html', {'form': form})
+
+
+def patient_detail(request, patient_id):
+    
+    patient = Patient.objects.get(id=patient_id)
+    
+    if request.method == 'POST':
+        form = PatientForm(request.POST, instance=patient)
+        if form.is_valid():
+            form.save()
+            return redirect('patients_list')
+    else:
+        form = PatientForm(instance=patient)
+    
+    return render(request, 'patients/patient_detail.html', {
+        'patient': patient,
+        'form': form
+    })
