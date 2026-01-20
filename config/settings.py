0d0a9ceb43a425f888e43d0813a4c2907b278626
Diego Lozano <diegolozadev@gmail.com>
2026-01-20 13:10:17 -0500

    Add database configuration using dj_database_url and load environment variables

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +31,12 @@ SECRET_KEY = 'django-insecure-2uok+%re04g=io1dsx9hqxawkus842772-!#x@xc8e1g%^-*55
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
     '*',
 ]
 
+# CSRF Trusted Origins for deployment
 CSRF_TRUSTED_ORIGINS = [
     'http://*',
     'https://web-production-f66a7.up.railway.app'
@@ -86,11 +94,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+# Pegas aquí la "Public Connection String" que te da Railway
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Esto busca la variable de entorno llamada DATABASE_URL
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
